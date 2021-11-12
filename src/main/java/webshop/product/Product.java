24ad6ec4bd8073ee3bab0d849e92988909c8b641
gkevin94 <gkevin94@gmail.com>
2021-11-12 12:29:18 +0100

    created Product.java and added attributes

---
+++ src/main/java/webshop/product/Product.java
@@ -0,0 +1,12 @@
+package webshop.product;
+
+public class Product {
+
+    private long id;
+    private String code;
+    private String name;
+    private String address;
+    private String manufacturer;
+    private int price;
+    private ProductStatus productStatus;
+}
