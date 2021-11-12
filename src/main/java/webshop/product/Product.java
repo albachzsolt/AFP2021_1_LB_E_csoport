af3331ebc6b0d7a159334a148a539b24c4a48e95
gkevin94 <gkevin94@gmail.com>
2021-11-12 13:30:33 +0100

    created constructors for Product

--- src/main/java/webshop/product/Product.java
+++ src/main/java/webshop/product/Product.java
@@ -1,5 +1,7 @@
 package webshop.product;
 
+import java.text.Normalizer;
+
 public class Product {
 
     private long id;
@@ -9,4 +11,53 @@ public class Product {
     private String manufacturer;
     private int price;
     private ProductStatus productStatus;
+
+    public Product() {
+    }
+
+    public Product(long id, String code, String name, String manufacturer, int price,
+                   ProductStatus productStatus) {
+        this.id = id;
+        this.code = code;
+        this.name = deleteRedundantSpace(name.trim());
+        address = generateAddress();
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.productStatus = productStatus;
+    }
+
+    public Product(long id, String code, String name, String address, String manufacturer, int price,
+                   ProductStatus productStatus) {
+        this.id = id;
+        this.code = code;
+        this.name = deleteRedundantSpace(name.trim());
+        this.address = address;
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.productStatus = productStatus;
+    }
+
+    public String generateAddress(){
+        String newAddress = name.toLowerCase().replaceAll(" ", "_");
+        return normalize(newAddress);
+    }
+
+    private String normalize(String s) {
+        s = Normalizer.normalize(s, Normalizer.Form.NFD);
+        s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+        return s;
+    }
+
+    private String deleteRedundantSpace(String string){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < string.length()-1;i++){
+            if (!(string.charAt(i) == ' ' &&  string.charAt(i+1)==' ')){
+                stringBuilder.append(string.charAt(i));
+            }
+        }
+        if(string.equals("")){
+            return "";
+        }
+        return stringBuilder.append(string.charAt(string.length()-1)).toString();
+    }
 }
