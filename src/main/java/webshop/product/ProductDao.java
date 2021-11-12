1040f2608f951edca928d5c76f209b52d635dc01
gkevin94 <gkevin94@gmail.com>
2021-11-12 15:07:20 +0100

    created constructor in ProductDao.java

--- src/main/java/webshop/product/ProductDao.java
+++ src/main/java/webshop/product/ProductDao.java
@@ -1,4 +1,16 @@
 package webshop.product;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 public class ProductDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public ProductDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
 }
