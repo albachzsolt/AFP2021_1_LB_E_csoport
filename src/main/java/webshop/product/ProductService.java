ca9cf3c531515d888a4f47a949f9f0b051e88a75
gkevin94 <gkevin94@gmail.com>
2021-11-12 15:21:40 +0100

    implemented listAllProducts in ProductService.java

--- src/main/java/webshop/product/ProductService.java
+++ src/main/java/webshop/product/ProductService.java
@@ -1,4 +1,19 @@
 package webshop.product;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private ProductDao productDao;
+
+    public ProductService(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    public List<Product> listAllProducts(){
+        return productDao.listAllProducts();
+    }
 }
