cf9f3e1ef088b04b71494e58ed9bf159f45e98dd
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-11-13 20:26:48 +0100

    Stat: created fields with constructor

--- src/main/java/webshop/statics/Stat.java
+++ src/main/java/webshop/statics/Stat.java
@@ -1,4 +1,19 @@
 package webshop.statics;
 
 public class Stat {
+
+    private int numOfUsers;
+    private int numOfActiveProducts;
+    private int numOfProducts;
+    private int numOfActiveOrders;
+    private int numOfOrders;
+
+
+    public Stat(int numOfUsers, int numOfActiveProducts, int numOfProducts, int numOfActiveOrders, int numOfOrders) {
+        this.numOfUsers = numOfUsers;
+        this.numOfActiveProducts = numOfActiveProducts;
+        this.numOfProducts = numOfProducts;
+        this.numOfActiveOrders = numOfActiveOrders;
+        this.numOfOrders = numOfOrders;
+    }
 }
