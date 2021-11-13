bf015d0520dfeab3d1823f297e8148bbc4a60260
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-13 17:25:14 +0100

    created User.java and added attributes

---
+++ src/main/java/webshop/user/User.java
@@ -0,0 +1,10 @@
+package webshop.user;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+}
