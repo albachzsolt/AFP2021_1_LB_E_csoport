acfd1c9fff76720549efc19d85419b4578ff8ba6
gkevin94 <gkevin94@gmail.com>
2021-11-14 10:41:17 +0100

    implemented address validator methods

--- src/main/java/webshop/product/ProductValidator.java
+++ src/main/java/webshop/product/ProductValidator.java
@@ -2,5 +2,25 @@ package webshop.product;
 
 import webshop.Validator;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProductValidator implements Validator {
+
+    private ProductService productService;
+
+    public ProductValidator(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public boolean isValidAddress(String address){
+        List<String> addresses = productService.listAllProducts().stream().map(Product::getAddress).collect(Collectors.toList());
+        return address != null && !address.trim().equals("") && addresses.contains(address);
+    }
+
+    public void isAddressNull(Product product){
+        if (product.getAddress() == null || "".equals(product.getAddress().trim())){
+            product.setAddress(product.generateAddress());
+        }
+    }
 }
