a48d9703c646be298643d9ed2503b913c6c4dd79
gkevin94 <gkevin94@gmail.com>
2021-11-15 20:59:02 +0100

    added 2 attributes, constructor and getters to BasketItem.java

--- src/main/java/webshop/basket/BasketItem.java
+++ src/main/java/webshop/basket/BasketItem.java
@@ -1,4 +1,23 @@
 package webshop.basket;
 
+import webshop.product.Product;
+
 public class BasketItem {
+
+    private Product product;
+    private int pieces;
+
+    public BasketItem(Product product, int pieces) {
+        this.product = product;
+        this.pieces = pieces;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getPieces() {
+        return pieces;
+    }
+
 }
