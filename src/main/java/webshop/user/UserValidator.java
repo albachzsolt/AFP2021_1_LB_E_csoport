09449b48ac741b396c066bf164abf57105acf05f
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-16 13:36:51 +0100

    implemented user validator methods

--- src/main/java/webshop/user/UserValidator.java
+++ src/main/java/webshop/user/UserValidator.java
@@ -1,4 +1,17 @@
 package webshop.user;
 
-public class UserValidator {
+import org.springframework.stereotype.Component;
+import webshop.Validator;
+
+@Component
+public class UserValidator implements Validator{
+
+    public boolean userCanBeUpdated(User user) {
+        return !isEmpty(user.getFirstName()) && !isEmpty(user.getLastName()) && isUsernameValid(user) &&
+                !isEmpty(user.getPassword());
+    }
+
+    private boolean isUsernameValid(User user) {
+        return !isEmpty(user.getUsername()) && !user.getUsername().toUpperCase().contains("DELETED_USER");
+    }
 }
