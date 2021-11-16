de2fb0ee67276dbfdbf55e935bc51255cb7482e7
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-16 13:45:55 +0100

    created constructor and added getters and setters to UserData.java

--- src/main/java/webshop/user/UserData.java
+++ src/main/java/webshop/user/UserData.java
@@ -5,5 +5,24 @@ public class UserData {
     private String username;
     private UserRole role;
 
+    public UserData(String username, UserRole role) {
+        this.username = username;
+        this.role = role;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
