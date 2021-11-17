0ed4b2337ca814f32af3a81925cb286ef77e8e4f
gkevin94 <gkevin94@gmail.com>
2021-11-17 17:06:07 +0100

    implemented addNewCategoryAndGetId in ProductDao.java

--- src/main/java/webshop/category/CategoryDao.java
+++ src/main/java/webshop/category/CategoryDao.java
@@ -1,11 +1,13 @@
 package webshop.category;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 @Repository
@@ -42,5 +44,24 @@ public class CategoryDao {
                         resultSet.getInt(SEQUENCE));
             }
         }, category.getCategoryName());
+    }
+
+    public long addNewCategoryAndGetId(Category category){
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+                                @Override
+                                public PreparedStatement createPreparedStatement(Connection connection)
+                                        throws SQLException {
+                                    PreparedStatement ps =
+                                            connection.prepareStatement("insert into categories (name, sequence) values (?, ?)",
+                                                    Statement.RETURN_GENERATED_KEYS);
+                                    ps.setString(1, category.getCategoryName());
+                                    ps.setInt(2, category.getSequence());
+                                    return ps;
+                                }
+                            }, keyHolder
+        );
+        return keyHolder.getKey().longValue();
     }
 }
