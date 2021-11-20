4e826291af68814729545ef41151130541955178
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-20 14:35:30 +0100

    created constructor in UserDao.java

--- src/main/java/webshop/user/UserDao.java
+++ src/main/java/webshop/user/UserDao.java
@@ -1,4 +1,15 @@
 package webshop.user;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 public class UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private static final String USERNAME = "username";
+
+    public UserDao(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 }
