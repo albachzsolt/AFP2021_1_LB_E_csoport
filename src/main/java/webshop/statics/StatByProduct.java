b7a6052f81484503bf0a6ca91b4d197592d9c59e
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-11-20 19:00:26 +0100

    created StatByProduct class, generated getters

---
+++ src/main/java/webshop/statics/StatByProduct.java
@@ -0,0 +1,35 @@
+package webshop.statics;
+
+public class StatByProduct {
+    private int year;
+    private int month;
+    private String productName;
+    private int productPrice;
+    private int prodcutCounter;
+    private int amount;
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public int getProdcutCounter() {
+        return prodcutCounter;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
