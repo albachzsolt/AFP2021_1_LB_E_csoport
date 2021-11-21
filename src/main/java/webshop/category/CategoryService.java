aaf35e1c0db00382da34041e2d57de6288898d78
gkevin94 <gkevin94@gmail.com>
2021-11-21 18:59:11 +0100

    implemented listAllProducts in CategoryService.java

--- src/main/java/webshop/category/CategoryService.java
+++ src/main/java/webshop/category/CategoryService.java
@@ -3,6 +3,8 @@ package webshop.category;
 import org.springframework.stereotype.Service;
 import webshop.product.ProductDao;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
 
@@ -12,5 +14,14 @@ public class CategoryService {
     public CategoryService(CategoryDao categoryDao, ProductDao productDao) {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
+    }
+
+    public List<Category> listAllProducts(){
+        List<Category> categoryList = categoryDao.listAllCategories();
+
+        for (Category category : categoryList){
+            category.setProducts(productDao.listAllProductsByCategory(category));
+        }
+        return categoryList;
     }
 }
