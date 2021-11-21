5682d6e911ee45b15df98ba99cdc0e5632deac35
gkevin94 <gkevin94@gmail.com>
2021-11-21 11:56:59 +0100

    implemented validateProduct in ProductValidator.java

--- src/main/java/webshop/product/ProductValidator.java
+++ src/main/java/webshop/product/ProductValidator.java
@@ -1,5 +1,7 @@
 package webshop.product;
 
+import webshop.CustomResponseStatus;
+import webshop.Response;
 import webshop.Validator;
 
 import java.util.List;
@@ -11,6 +13,16 @@ public class ProductValidator implements Validator {
 
     public ProductValidator(ProductService productService) {
         this.productService = productService;
+    }
+
+    public CustomResponseStatus validateProduct(Product product){
+        if (isEmpty(product.getName()) || isEmpty(product.getCode()) || isEmpty(product.getManufacturer()) || product.getPrice() == 0){
+            return new CustomResponseStatus(Response.FAILED, "These parameters can not be empty: code, name, manufacturer, price");
+        }
+        if (product.getPrice() > 2_000_000 || product.getPrice() < 0){
+            return new CustomResponseStatus(Response.FAILED, "Price must be between 0 and 2M Ft.");
+        }
+        return new CustomResponseStatus(Response.SUCCESS, "Product is okay.");
     }
 
     public boolean isValidAddress(String address){
