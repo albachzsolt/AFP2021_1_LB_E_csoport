33afce74da4bb52f3eb56954679f64b21dbf3cc6
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-23 12:32:01 +0100

    created constructor in UserService.java

--- src/main/java/webshop/user/UserService.java
+++ src/main/java/webshop/user/UserService.java
@@ -1,5 +1,20 @@
 package webshop.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import webshop.basket.BasketDao;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private BasketDao basketDao;
+
+    public UserService(UserDao userDao, BasketDao basketDao) {
+        this.userDao = userDao;
+        this.basketDao = basketDao;
+    }
 }
