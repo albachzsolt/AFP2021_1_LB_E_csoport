aee33a2a9a4ca93c2d482e515a966ce302c4410e
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-23 14:27:57 +0100

    created attributes for Order.java

--- src/main/java/webshop/order/Order.java
+++ src/main/java/webshop/order/Order.java
@@ -1,4 +1,12 @@
 package webshop.order;
 
+import java.time.LocalDateTime;
+
 public class Order {
+    private long id;
+    private long userId;
+    private LocalDateTime orderTime;
+    private OrderStatus orderStatus;
+    private long totalOrderPrice;
+    private String shippingAddress;
 }
