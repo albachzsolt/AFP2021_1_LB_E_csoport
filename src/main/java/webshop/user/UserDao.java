53cdc1be1efd4fa21977b93be2ebe7ef3ccfcd18
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-23 10:58:32 +0100

    implemented createUserAndReturnUserId in UserDao.java

--- src/main/java/webshop/user/UserDao.java
+++ src/main/java/webshop/user/UserDao.java
@@ -1,9 +1,20 @@
 package webshop.user;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
+@Repository
 public class UserDao {
 
     private JdbcTemplate jdbcTemplate;
@@ -11,5 +22,24 @@ public class UserDao {
 
     public UserDao(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public long createUserAndReturnUserId(User user) throws DuplicateKeyException {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("insert into users " + "(first_name, last_name, username, " + "password, enabled, role) values " + "(?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, user.getFirstName());
+                ps.setString(2, user.getLastName());
+                ps.setString(3, user.getUsername());
+                ps.setString(4, user.getPassword());
+                ps.setInt(5, user.getEnabled());
+                ps.setString(6, user.getUserRole().name());
+                return ps;
+            }
+            }, keyHolder
+        );
+        return keyHolder.getKey().longValue();
     }
 }
