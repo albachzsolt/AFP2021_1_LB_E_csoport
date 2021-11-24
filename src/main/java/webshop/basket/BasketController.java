e866b0b8e4091a469c53c95c25340e065bf9f43f
gkevin94 <gkevin94@gmail.com>
2021-11-24 19:03:01 +0100

    implemented getBasketDataForActualUser in BasketController

--- src/main/java/webshop/basket/BasketController.java
+++ src/main/java/webshop/basket/BasketController.java
@@ -1,6 +1,11 @@
 package webshop.basket;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import webshop.user.UserData;
+import webshop.user.UserRole;
 
 @RestController
 public class BasketController {
@@ -10,4 +15,19 @@ public class BasketController {
     public BasketController(BasketService basketService) {
         this.basketService = basketService;
     }
+
+    @GetMapping(value = "/basket")
+    @ResponseBody
+    public BasketData getBasketDataForActualUser(Authentication authentication) {
+
+        if (authentication != null) {
+            String loggedInUsername = authentication.getName();
+            return basketService.getBasketDataByUser(loggedInUsername);
+        } else {
+            return (new BasketData(0, 0, new Basket(0, new UserData("",
+                    UserRole.NOT_AUTHENTICATED))));
+        }
+    }
+
+
 }
