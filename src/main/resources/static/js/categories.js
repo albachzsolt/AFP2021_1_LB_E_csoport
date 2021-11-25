967a305a098fdf56ab49a91218c2cb4167d072e0
gkevin94 <gkevin94@gmail.com>
2021-11-25 19:59:04 +0100

    implemented showCategories function

--- src/main/resources/static/js/categories.js
+++ src/main/resources/static/js/categories.js
@@ -5,3 +5,23 @@ function getCategories() {
         .then(response => response.json())
         .then(json => showCategories(json));
 }
+
+function showCategories(jsonData) {
+    var ul = document.getElementById('sortable');
+    ul.innerHTML = '';
+    for (const key in jsonData) {
+        if (jsonData.hasOwnProperty(key)) {
+            const e = jsonData[key];
+            var id = jsonData[key].id;
+            var liId = 'li-' + id;
+            ul.innerHTML += `
+      <li id=${liId}>
+      <img src="img/dragNdrop.png" alt="dragNdrop" class="handle" height="25">
+      <label>${e.sequence}</label>
+      <label contenteditable="true">${e.categoryName}</label>
+      <img id=${id} src="/img/delete-button.png" onclick="deleteCategory()" class='category-delete'>
+      </li>
+      `;
+        }
+    }
+}
