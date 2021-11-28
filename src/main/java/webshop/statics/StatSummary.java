ceaef28dd8870e66a8e53e327f61d009fb7d3bfd
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-11-28 21:34:08 +0100

    created StatSummary class

---
+++ src/main/java/webshop/statics/StatSummary.java
@@ -0,0 +1,12 @@
+package webshop.statics;
+
+import webshop.order.OrderStatus;
+
+public class StatSummary {
+
+    private OrderStatus orderStatus;
+    private int piece;
+    private long amount;
+
+
+}
