1175fa3c5c1fc6cb8740ae02d82aa6b8d29d5246
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-11-28 21:57:56 +0100

    generated StatReportOne class

---
+++ src/main/java/webshop/statics/StatReportOne.java
@@ -0,0 +1,9 @@
+package webshop.statics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatReportOne {
+
+    private List<StatusOrderReport> statusOrderReportList = new ArrayList<>();
+}
