48d8c778514ba76e07ca6f8e5b572d8c37ceccab
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-29 12:38:27 +0100

    implemented placeOrder in OrderService.java

--- src/main/java/webshop/order/OrderService.java
+++ src/main/java/webshop/order/OrderService.java
@@ -1,8 +1,14 @@
 package webshop.order;
 
 import org.springframework.stereotype.Service;
+import webshop.CustomResponseStatus;
+import webshop.Response;
 import webshop.basket.BasketDao;
+import webshop.basket.BasketItem;
+import webshop.user.User;
 import webshop.user.UserDao;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -14,5 +20,22 @@ public class OrderService {
         this.orderDao = orderDao;
         this.userDao = userDao;
         this.basketDao = basketDao;
+    }
+
+    public CustomResponseStatus placeOrder(String loggedInUsername, String shippingAddress) {
+        User user = userDao.getUserByUsername(loggedInUsername);
+        long userId = user.getId();
+        long basketId = basketDao.getBasketIdByUserId(userId);
+        List<BasketItem> products = basketDao.getBasketItemsInBasketByBasketId(basketId);
+        if (products.isEmpty()) {
+            return new CustomResponseStatus(Response.FAILED, "Your cart is empty.");
+        }
+        long orderId = orderDao.insertIntoOrdersFromBasketsByUserId(userId, shippingAddress);
+        for (BasketItem basketItem : products) {
+            orderDao.insertIntoOrderedItemsFromBasketItemsByOrderId(orderId, basketItem.getProduct().getId(),
+                    basketItem.getPieces(), basketItem.getProduct().getPrice() * basketItem.getPieces());
+        }
+        basketDao.clearBasketByBasketId(basketId);
+        return new CustomResponseStatus(Response.SUCCESS, "Your order was placed, thank you for your purchase.");
     }
 }
