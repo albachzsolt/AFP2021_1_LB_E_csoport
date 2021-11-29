cf29398f5179f1e43b9bcce18b075888d5274f1a
Tamás Fodor <fodor.tamas48@gmail.com>
2021-11-29 13:12:40 +0100

    implemented getOrderDataForActualUser inOrderController.java

--- src/main/java/webshop/order/OrderController.java
+++ src/main/java/webshop/order/OrderController.java
@@ -1,6 +1,12 @@
 package webshop.order;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import webshop.CustomResponseStatus;
+import webshop.Response;
 
 @RestController
 public class OrderController {
@@ -9,5 +15,32 @@ public class OrderController {
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @PostMapping("/myorders")
+    @ResponseBody
+    public CustomResponseStatus getOrderDataForActualUser(Authentication authentication,
+                                                          @RequestBody Order orderWithShippingAddressOnly) {
+        if (authentication != null) {
+            String shippingAddress;
+            if (orderWithShippingAddressOnly == null) {
+                shippingAddress = "";
+            }
+            else {
+                shippingAddress = orderWithShippingAddressOnly.getShippingAddress();
+            }
+            if (orderValidator.isEmpty(shippingAddress)) {
+                return new CustomResponseStatus(Response.FAILED, "Shipping address cannot be empty.");
+            }
+            String loggedInUsername = authentication.getName();
+            if (orderService.isShippingAddressAlreadyStored(loggedInUsername, shippingAddress)) {
+                return new CustomResponseStatus(Response.FAILED, "Shipping address already stored. " +
+                        "Please choose from the list below.");
+            }
+            return orderService.placeOrder(loggedInUsername, shippingAddress);
+        }
+        else {
+            return new CustomResponseStatus(Response.FAILED, "Please log in to order.");
+        }
     }
 }
