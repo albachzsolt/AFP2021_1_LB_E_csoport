1e166ee75faf9c2320504d888266229e05f7c44e
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-12-01 20:41:32 +0100

    StatData: updated fields with OrderStatus, generated toString method

--- src/main/java/webshop/statics/StatData.java
+++ src/main/java/webshop/statics/StatData.java
@@ -1,14 +1,19 @@
 package webshop.statics;
 
+import webshop.order.OrderStatus;
+
 public class StatData {
     private int year;
     private int month;
+    private OrderStatus orderStatus;
     private int piece;
     private int amount;
 
-    public StatData(int year, int month, int piece, int amount) {
+
+    public StatData(int year, int month, OrderStatus orderStatus, int piece, int amount) {
         this.year = year;
         this.month = month;
+        this.orderStatus = orderStatus;
         this.piece = piece;
         this.amount = amount;
     }
@@ -21,11 +26,26 @@ public class StatData {
         return month;
     }
 
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
     public int getPiece() {
         return piece;
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "StatData{" +
+                "year=" + year +
+                ", month=" + month +
+                ", orderStatus=" + orderStatus +
+                ", piece=" + piece +
+                ", amount=" + amount +
+                '}';
     }
 }
