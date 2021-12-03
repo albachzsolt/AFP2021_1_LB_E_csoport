c0ef60d21731faf22c898a04cb33ed4a1690439f
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-12-03 20:42:48 +0100

    reports.js: fixed problems

--- src/main/resources/static/js/reports.js
+++ src/main/resources/static/js/reports.js
@@ -2,7 +2,7 @@ fetchReports();
 
 
 function fetchReports() {
-    var url = "/reports/orders";
+    const url = "/reports/orders";
     fetch(url)
         .then(function (response) {
             return response.json();
@@ -13,78 +13,86 @@ function fetchReports() {
 }
 
 function showTables(jsonData) {
-    document.getElementById("report-table").innerHTML = "";
-    tableMain = document.getElementById("report-table");
-    for (var i = 0; i < jsonData["statusOrderReportList"].length; i++) {
-        var oneRow = document.createElement("tr");
+    let delAmountTd;
+    let delPieceTd;
+    let deliAmountTd;
+    let actPieceTd;
+    let deliPieceTd;
+    let oneRow;
+    let actAmountTd;
 
-        var yearTd = document.createElement("td");
+    document.getElementById("report-table").innerHTML = "";
+    let tableMain = document.getElementById("report-table");
+    for (let i = 0; i < jsonData["statusOrderReportList"].length; i++) {
+        oneRow = document.createElement("tr");
+
+        const yearTd = document.createElement("td");
         yearTd.innerHTML = jsonData["statusOrderReportList"][i].year;
         oneRow.appendChild(yearTd);
 
-        var monthTd = document.createElement("td");
+        const monthTd = document.createElement("td");
         monthTd.innerHTML = jsonData["statusOrderReportList"][i].month;
         oneRow.appendChild(monthTd);
 
 
-        var actPieceTd = document.createElement("td");
+        actPieceTd = document.createElement("td");
         actPieceTd.innerHTML = jsonData["statusOrderReportList"][i].sumOfActiveOrdersForThisMonth;
         oneRow.appendChild(actPieceTd);
 
-        var actAmountTd = document.createElement("td");
+        actAmountTd = document.createElement("td");
         actAmountTd.innerHTML = jsonData["statusOrderReportList"][i].sumOfAmountForActiveOrdersForThisMonth;
         oneRow.appendChild(actAmountTd);
 
-        var deliPieceTd = document.createElement("td");
+        deliPieceTd = document.createElement("td");
         deliPieceTd.innerHTML = jsonData["statusOrderReportList"][i].sumOfDeliveredOrdersForThisMonth;
         oneRow.appendChild(deliPieceTd);
 
-        var deliAmountTd = document.createElement("td");
+        deliAmountTd = document.createElement("td");
         deliAmountTd.innerHTML = jsonData["statusOrderReportList"][i].sumOfAmountForDeliveredOrdersForThisMonth;
         oneRow.appendChild(deliAmountTd);
 
-        var delPieceTd = document.createElement("td");
+        delPieceTd = document.createElement("td");
         delPieceTd.innerHTML = jsonData["statusOrderReportList"][i].sumOfDeletedOrdersForThisMonth;
         oneRow.appendChild(delPieceTd);
 
-        var delAmountTd = document.createElement("td");
+        delAmountTd = document.createElement("td");
         delAmountTd.innerHTML = jsonData["statusOrderReportList"][i].sumOfAmountForDeletedOrdersForThisMonth;
         oneRow.appendChild(delAmountTd);
 
         tableMain.appendChild(oneRow);
 
     }
-    var oneRow = document.createElement("tr");
+    oneRow = document.createElement("tr");
 
-    var emptyTd = document.createElement("td");
+    const emptyTd = document.createElement("td");
     emptyTd.innerHTML = "Orders";
     oneRow.appendChild(emptyTd);
 
-    var emptyTd2 = document.createElement("td");
+    const emptyTd2 = document.createElement("td");
     emptyTd2.innerHTML = "Summary";
     oneRow.appendChild(emptyTd2);
 
-    var actPieceTd = document.createElement("td");
+    actPieceTd = document.createElement("td");
     actPieceTd.innerHTML = jsonData["statRowSummary"].actPiece;
     oneRow.appendChild(actPieceTd);
 
-    var actAmountTd = document.createElement("td");
+    actAmountTd = document.createElement("td");
     actAmountTd.innerHTML = jsonData["statRowSummary"].actAmount;
     oneRow.appendChild(actAmountTd);
 
-    var deliPieceTd = document.createElement("td");
+    deliPieceTd = document.createElement("td");
     deliPieceTd.innerHTML = jsonData["statRowSummary"].deliPiece;
     oneRow.appendChild(deliPieceTd);
 
-    var deliAmountTd = document.createElement("td");
+    deliAmountTd = document.createElement("td");
     deliAmountTd.innerHTML = jsonData["statRowSummary"].deliAmount;
     oneRow.appendChild(deliAmountTd);
 
-    var delPieceTd = document.createElement("td");
+    delPieceTd = document.createElement("td");
     delPieceTd.innerHTML = jsonData["statRowSummary"].delPiece;
     oneRow.appendChild(delPieceTd);
 
-    var delAmountTd = document.createElement("td");
+    delAmountTd = document.createElement("td");
     delAmountTd.innerHTML = jsonData["statRowSummary"].delAmount;
     oneRow.appendChild(delAmountTd);
 
