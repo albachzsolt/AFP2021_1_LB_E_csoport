fef859e7af18d2ee66a7acc5751d5db4677a00eb
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-12-03 21:21:17 +0100

    WebshopApplication: implemented http security configure method

--- src/main/java/webshop/WebshopApplication.java
+++ src/main/java/webshop/WebshopApplication.java
@@ -2,12 +2,41 @@ package webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class WebshopApplication {
+@EnableWebSecurity
+@Configuration
+public class WebshopApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebshopApplication.class, args);
 	}
 
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/", "/register.html",
+						"product.html**", "/js/**", "/css/**", "/img/**", "/fonts/**", "/userdata", "/basket", "/users", "/api" +
+								"/categories", "/api/category/**", "/api/product/recommend").permitAll()
+				.antMatchers("/basket.html", "/orders", "/myorders", "/myorders/storedaddresses", "/orders/shippingaddresses",
+						"/basket", "/basket/update", "/basketitem/**").authenticated()
+				.antMatchers("/myorders.html", "/profile.html", "/api/user/**").hasRole("USER")
+				.antMatchers("order.html", "/orders.html", "/dashboard.html", "/users.html", "/adminproducts.html", "/dashboard",
+						"/reports/**", "/api/users", "/api/users/**", "api/products", "api/product/**",
+						"/orders/**", "orders/**/**", "/orders/filtered/**", "/basket/**", "/categories.html", "api/categories/update").hasRole(
+						"ADMIN")
+				.and()
+				.formLogin().loginPage("/login.html")
+				.loginProcessingUrl("/login")
+				.and()
+				.logout()
+				.logoutSuccessUrl("/");
+	}
 }
