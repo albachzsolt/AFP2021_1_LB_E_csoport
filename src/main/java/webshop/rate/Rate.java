bcb8432fa1863f05ddffc1560bd827db9da7717f
Tamás Fodor <fodor.tamas48@gmail.com>
2021-12-04 23:53:03 +0100

    generated constructors and implemented changeNullDate in Rate.java

--- src/main/java/webshop/rate/Rate.java
+++ src/main/java/webshop/rate/Rate.java
@@ -12,4 +12,28 @@ public class Rate {
     private LocalDate date;
     private User user;
     private Product product;
+
+    public Rate() {
+    }
+
+    public Rate( User user, Product product) {
+        this.user = user;
+        this.product = product;
+    }
+
+    public Rate(long id, String message, int stars, LocalDate date, User user, Product product) {
+        this.id = id;
+        this.message = message;
+        this.stars = stars;
+        this.date = changeNullDate(date);
+        this.user = user;
+        this.product = product;
+    }
+
+    private LocalDate changeNullDate(LocalDate date){
+        if (date == null){
+            return LocalDate.now();
+        }
+        return date;
+    }
 }
