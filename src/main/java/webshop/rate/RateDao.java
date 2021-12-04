8a1dcb7f0c9e813b5ca2607e39b91d9d8dbcd716
Tamás Fodor <fodor.tamas48@gmail.com>
2021-12-05 00:12:29 +0100

    implemented addNewRateAndGetId in RateDao.java

--- src/main/java/webshop/rate/RateDao.java
+++ src/main/java/webshop/rate/RateDao.java
@@ -2,11 +2,15 @@ package webshop.rate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import webshop.product.ProductDao;
 import webshop.user.UserDao;
 
 import javax.sql.DataSource;
+import java.sql.*;
 
 @Repository
 public class RateDao {
@@ -18,5 +22,24 @@ public class RateDao {
 
     public RateDao(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public long addNewRateAndGetId(Rate rate) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        Date date = Date.valueOf(rate.getDate());
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("insert into ratings (user_id, product_id, " +
+                        "stars, message, rating_time) values (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+                ps.setLong(1, rate.getUser().getId());
+                ps.setLong(2, rate.getProduct().getId());
+                ps.setInt(3, rate.getStars());
+                ps.setString(4, rate.getMessage());
+                ps.setDate(5, date);
+                return ps;
+            }
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
     }
 }
