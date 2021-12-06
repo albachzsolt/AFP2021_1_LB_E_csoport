ae89d8ffff7395fdf3bea8e9e818e1bacb35b75a
Tamás Fodor <fodor.tamas48@gmail.com>
2021-12-06 12:07:51 +0100

    implemented getRatesForProduct and getAvgRatesForProduct in RateController.java

--- src/main/java/webshop/rate/RateController.java
+++ src/main/java/webshop/rate/RateController.java
@@ -1,9 +1,14 @@
 package webshop.rate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import webshop.product.Product;
 import webshop.product.ProductService;
 import webshop.user.UserService;
+
+import java.util.List;
 
 @RestController
 public class RateController {
@@ -13,4 +18,16 @@ public class RateController {
     private UserService userService;
     @Autowired
     private ProductService productService;
+
+    @GetMapping("/api/rating/list/{productid}")
+    public List<Rate> getRatesForProduct(@PathVariable long productid) {
+        Product product = productService.getProductByProductId(productid);
+        return rateService.getRatesForProduct(product);
+    }
+
+    @GetMapping("/api/rating/avg/{productid}")
+    public double getAvgRatesForProduct(@PathVariable long productid) {
+        Product product = productService.getProductByProductId(productid);
+        return rateService.getAvgRatesForProduct(product);
+    }
 }
