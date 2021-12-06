7807d9685419e179c443b10a29a7974794761a46
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2021-12-06 21:25:37 +0100

    implemented scrollFunction in products.js

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -143,3 +143,20 @@ var mainDiv = document.getElementById('main_div');
 mainDiv.prepend(selectAllButton);
 mainDiv.prepend(select);
 }
+
+
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
