e44d979441887f643d1c67e37330aeb4d4da546b
Tamás Fodor <fodor.tamas48@gmail.com>
2021-12-06 12:28:16 +0100

    implemented addRate in RateController.java

--- src/main/java/webshop/rate/RateController.java
+++ src/main/java/webshop/rate/RateController.java
@@ -2,9 +2,7 @@ package webshop.rate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import webshop.CustomResponseStatus;
 import webshop.Response;
 import webshop.product.Product;
@@ -12,6 +10,7 @@ import webshop.product.ProductService;
 import webshop.user.User;
 import webshop.user.UserService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -57,5 +56,19 @@ public class RateController {
         return new RateWithStatus(new CustomResponseStatus(Response.FAILED, "Not logged in!"), new Rate());
     }
 
-
+    @PostMapping("/api/rating/userrating/{id}")
+    public CustomResponseStatus addRate(Authentication authentication, @PathVariable long id, @RequestBody Rate rate) {
+        if (authentication != null) {
+            if (rate.getStars() > 0) {
+                String loggedInUsername = authentication.getName();
+                User loggedInUser = userService.getUserByUsername(loggedInUsername);
+                rate.setUser(loggedInUser);
+                rate.setDate(LocalDate.now());
+                return rateService.addRate(rate);
+            } else {
+                return new CustomResponseStatus(Response.FAILED,"Please add min 1 star!");
+            }
+        }
+        return new CustomResponseStatus(Response.FAILED, "Please sign in to rate.");
+    }
 }
