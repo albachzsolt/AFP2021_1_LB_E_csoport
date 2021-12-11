50256cf6c9589d029c356bf126a0a993e7ac51ed
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-12-11 20:11:41 +0100

    updated reports.js

--- src/main/resources/static/js/reports.js
+++ src/main/resources/static/js/reports.js
@@ -98,3 +98,38 @@ function showTables(jsonData) {
 
     tableMain.appendChild(oneRow);
 }
+
+function showTables2(jsonData) {
+    document.getElementById("report-table_prod").innerHTML = "";
+    tableMain = document.getElementById("report-table_prod");
+    for (var i = 0; i < jsonData.length; i++) {
+        var oneRow = document.createElement("tr");
+
+        var yearTd = document.createElement("td");
+        yearTd.innerHTML = jsonData[i].year;
+        oneRow.appendChild(yearTd);
+
+        var monthTd = document.createElement("td");
+        monthTd.innerHTML = jsonData[i].month;
+        oneRow.appendChild(monthTd);
+
+
+        var productTd = document.createElement("td");
+        productTd.innerHTML = jsonData[i].productName;
+        oneRow.appendChild(productTd);
+
+        var priceTd = document.createElement("td");
+        priceTd.innerHTML = jsonData[i].productPrice;
+        oneRow.appendChild(priceTd);
+
+        var countTd = document.createElement("td");
+        countTd.innerHTML = jsonData[i].productCounter;
+        oneRow.appendChild(countTd);
+
+        var amountTd = document.createElement("td");
+        amountTd.innerHTML = jsonData[i].amount;
+        oneRow.appendChild(amountTd);
+
+        tableMain.appendChild(oneRow);
+    }
+}
