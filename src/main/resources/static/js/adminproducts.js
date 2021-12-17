ea858c42f222c8fb5f917614bfc9a2665d8af59d
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2021-12-17 09:00:19 +0100

    implemented categories in adminproducts.js

--- src/main/resources/static/js/adminproducts.js
+++ src/main/resources/static/js/adminproducts.js
@@ -15,3 +15,18 @@ function fetchProducts() {
       showDivs(jsonData);
     });
 }
+
+function fetchCategories(e){
+  var url = '/api/categories';
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      if (modify){
+      editItem(jsonData, e.target.id);
+      } else {
+      showInputFields(jsonData);
+      }
+    });
+}
