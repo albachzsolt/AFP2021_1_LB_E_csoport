897b5ee2be43ed5f031c073f90b83d68c3a1fb1d
Artur Pamlenyi <artur.pamlenyi@gmail.com>
2021-12-17 09:27:02 +0100

    implemented editBasketItemQuantity and orderItems in basket.js

--- src/main/resources/static/js/basket.js
+++ src/main/resources/static/js/basket.js
@@ -176,3 +176,180 @@ function deleteFromBasket() {
       fetchBasket();
     });
 }
+
+
+function editBasketItemQuantity() {
+  if (document.querySelectorAll('.save-enabled').length < 1) {
+    var data = this['raw-data'];
+    var id = data.product.id;
+
+    var editBtn = document.getElementById('edit' + id);
+    editBtn.setAttribute('class', 'button-disabled button edit-button edit-disabled');
+
+    var saveBtn = document.getElementById('save' + id);
+    saveBtn.setAttribute('class', 'button-enabled button save-button save-enabled');
+
+
+    var quantityTd = document.getElementById('quantityTd' + id);
+    quantityTd.innerHTML = `
+                        <div><input class="quantity-input" type="number" name="quantityinput" id="quantityinput${id}" min="1" value="${data.pieces}"></div>
+  `;
+  }
+}
+
+
+function saveUpdatedItemQuantity() {
+  var data = this['raw-data'];
+  var id = data.product.id;
+  var code = data.product.code;
+
+  var quantity = document.getElementById('quantityinput' + id).value;
+
+  var saveBtn = document.getElementById('save' + id);
+  saveBtn.setAttribute('class', 'button-disabled button save-button save-disabled');
+
+  var editBtn = document.getElementById('edit' + id);
+  editBtn.setAttribute('class', 'button-enabled button edit-button edit-enabled');
+
+  var quantityTd = document.getElementById('quantityTd' + id);
+  quantityTd.innerHTML = quantity;
+
+  var request = {
+    'productCode': code,
+    'productPieces': quantity
+  };
+  fetch('/basket/update', {
+      method: 'POST',
+      body: JSON.stringify(request),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then(function (response) {
+      return response.json();
+    }).then(function (jsonData) {
+      if (jsonData.response === 'SUCCESS') {
+        document.getElementById('message-div').setAttribute('class', 'alert alert-success');
+      } else {
+        document.getElementById('message-div').setAttribute('class', 'alert alert-danger');
+      }
+      document.getElementById('message-div').innerHTML = jsonData.message;
+      fetchBasket();
+    });
+}
+
+function showShippingAddressInputField() {
+  var shippinAddressButton = document.querySelector('#add-shipping-address');
+  var shippingAddressField = document.querySelector('#shipping-address');
+  var shippingAddressChooserDiv = document.querySelector('#choose-address-div');
+  if (shippingAddressField.getAttribute('class') === 'disabled') {
+    shippingAddressField.setAttribute('class', 'enabled');
+    shippingAddressChooserDiv.setAttribute('class', 'disabled');
+    shippinAddressButton.innerHTML = 'Choose address';
+  } else {
+    shippingAddressField.setAttribute('class', 'disabled');
+    shippingAddressChooserDiv.setAttribute('class', 'enabled');
+    shippinAddressButton.innerHTML = 'Add address';
+  }
+}
+
+function fetchFormerShippingAddressList() {
+  var url = '/orders/shippingaddresses';
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      showFormerShippingAddressList(jsonData);
+    });
+}
+
+function showFormerShippingAddressList(jsonData) {
+  var shippingAddressChooserDiv = document.querySelector('#choose-address-div');
+  shippingAddressChooserDiv.innerHTML = '';
+  var shippingAddressField = document.querySelector('#shipping-address');
+
+  if (jsonData.length > 0) {
+    shippingAddressField.setAttribute('class', 'disabled');
+    shippingAddressChooserDiv.setAttribute('class', 'enabled');
+    var chooseHeaderDiv = document.createElement('div');
+    chooseHeaderDiv.setAttribute('class', 'choose-header');
+    chooseHeaderDiv.innerText = 'Choose shipping address';
+    shippingAddressChooserDiv.appendChild(chooseHeaderDiv);
+
+    for (var i = 0; i < jsonData.length; i++) {
+      var addressChooserInput = document.createElement('input');
+      var id = 'oneFormerAddress' + (i + 1);
+      addressChooserInput.setAttribute('id', id);
+      addressChooserInput.setAttribute('name', 'oneFormerAddress');
+      addressChooserInput.setAttribute('type', 'radio');
+      addressChooserInput.setAttribute('class', 'address-input-radio');
+      var oneAddress = jsonData[i].shippingAddress;
+      addressChooserInput.value = oneAddress;
+      var labelElement = document.createElement('label');
+      labelElement.setAttribute('for', id);
+      labelElement.innerHTML = oneAddress;
+      var oneInputDiv = document.createElement('div');
+      oneInputDiv.setAttribute('class', 'address-chooser');
+      oneInputDiv.appendChild(addressChooserInput);
+      oneInputDiv.appendChild(labelElement);
+      shippingAddressChooserDiv.appendChild(oneInputDiv);
+    }
+    document.getElementById('oneFormerAddress1').checked = true;
+  }
+}
+
+
+function orderItems() {
+  if (!confirm('Are you sure you want to place your order?')) {
+    return;
+  }
+
+  var shippingAddressField = document.querySelector('#shipping-address');
+  var shippingAddressChooserDiv = document.querySelector('#choose-address-div');
+
+  var shippingAddress;
+  var url;
+  if (shippingAddressField.getAttribute('class') === 'enabled') {
+    shippingAddress = shippingAddressField.value;
+    url = '/myorders';
+  } else if (shippingAddressChooserDiv.getAttribute('class') === 'enabled') {
+    if (typeof document.forms.shippingAddressChoserForm.elements.oneFormerAddress === 'undefined') {
+      chosenAddress = '';
+    } else {
+      var chosenAddress = document.forms.shippingAddressChoserForm.elements.oneFormerAddress.value;
+    }
+    shippingAddress = chosenAddress;
+    url = '/myorders/storedaddresses';
+  }
+
+  var request = {
+    'shippingAddress': shippingAddress
+  };
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(request),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonData) {
+      if (jsonData.response === 'SUCCESS') {
+        document.getElementById('shipping-address').value = '';
+        document.getElementById('shipping-address').setAttribute('class', 'disabled');
+        fetchBasket();
+        document.getElementById('message-div').innerHTML = jsonData.message;
+        document.getElementById('message-div').setAttribute('class', 'alert alert-success');
+      } else {
+        document.getElementById('message-div').innerHTML = jsonData.message;
+        document.getElementById('message-div').setAttribute('class', 'alert alert-danger');
+        if (jsonData.message === 'Shipping address already stored. Please chose from the list below.') {
+          showShippingAddressInputField();
+        }
+      }
+    });
+}
