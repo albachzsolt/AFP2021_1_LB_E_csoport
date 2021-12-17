24721b352deb56a780ebe070abeb905e54dfcd1d
Tamás Fodor <fodor.tamas48@gmail.com>
2021-12-17 11:21:15 +0100

    added insert into orders table in V7.sql

---
+++ src/main/resources/db.migration/V7__insert_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO orders (user_id, order_time, status, shipping_address) VALUES
+    (3, '2021-11-03 09:02:33', 'ACTIVE', '3300 Eger, Eszterházy tér 1.'),
+    (3, '2021-11-09 09:02:33', 'ACTIVE', '3300 Eger, Eszterházy tér 1.'),
+    (3, '2021-11-06 09:02:33', 'ACTIVE', '3300 Eger, Eszterházy tér 1.'),
+    (3, '2021-11-05 09:02:33', 'DELETED', '3300 Eger, Eszterházy tér 1.'),
+    (3, '2021-11-23 09:02:33', 'DELIVERED', '3300 Eger, Eszterházy tér 1.'),
+    (3, '2021-11-27 09:02:33', 'DELIVERED', '3300 Eger, Eszterházy tér 1.'),
+    (3, '2021-11-27 09:02:33', 'DELIVERED', '3300 Eger, Eszterházy tér 1.'),
+    (2, '2021-11-26 09:02:33', 'DELIVERED', '3300 Eger, Leányka utca 12.'),
+    (2, '2021-11-24 09:02:33', 'DELIVERED', '3300 Eger, Leányka utca 12.'),
+    (2, '2021-11-25 09:02:33', 'DELIVERED', '3300 Eger, Leányka utca 12.');
+
