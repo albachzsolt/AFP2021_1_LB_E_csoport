fcfb833b31f303b7091db9a780bb7577148134c3
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2021-12-17 19:37:46 +0100

    updated WebshopApplication.java, reports2.js

--- src/main/java/webshop/WebshopApplication.java
+++ src/main/java/webshop/WebshopApplication.java
@@ -1,11 +1,18 @@
 package webshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -39,4 +46,19 @@ public class WebshopApplication extends WebSecurityConfigurerAdapter {
 				.logout()
 				.logoutSuccessUrl("/");
 	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth, DataSource dataSource, PasswordEncoder passwordEncoder) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+				.usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+				.authoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
+
+	}
+
 }
--- src/main/resources/static/js/reports2.js
+++ src/main/resources/static/js/reports2.js
@@ -2,7 +2,7 @@ window.onload = function () {
     fetchReportsTwo();
 
     function fetchReportsTwo() {
-        var url = "/reports/products";
+        let url = "/reports/products";
         fetch(url)
             .then(function (response) {
                 return response.json();
@@ -14,29 +14,29 @@ window.onload = function () {
 
     function showTables(jsonData) {
         tableMain = document.getElementById("report-table_prod");
-        for (var i = 0; i < jsonData.length; i++) {
-            var oneRow = document.createElement("tr");
+        for (let i = 0; i < jsonData.length; i++) {
+            let oneRow = document.createElement("tr");
 
-            var yearTd = document.createElement("td");
+            let yearTd = document.createElement("td");
             yearTd.innerHTML = jsonData[i].year;
             oneRow.appendChild(yearTd);
 
-            var monthTd = document.createElement("td");
+            let monthTd = document.createElement("td");
             monthTd.innerHTML = jsonData[i].month;
             oneRow.appendChild(monthTd);
-            var productTd = document.createElement("td");
+            let productTd = document.createElement("td");
             productTd.innerHTML = jsonData[i].productName;
             oneRow.appendChild(productTd);
 
-            var priceTd = document.createElement("td");
+            let priceTd = document.createElement("td");
             priceTd.innerHTML = jsonData[i].productPrice;
             oneRow.appendChild(priceTd);
 
-            var countTd = document.createElement("td");
+            let countTd = document.createElement("td");
             countTd.innerHTML = jsonData[i].productCounter;
             oneRow.appendChild(countTd);
 
-            var amountTd = document.createElement("td");
+            let amountTd = document.createElement("td");
             amountTd.innerHTML = jsonData[i].amount;
             oneRow.appendChild(amountTd);
 
